b37df5f0f6c53b3d6744c57067e823e19a6a9078
Martin Pocock <pocockm@shernet.sheridancollege.ca>
2024-01-17 15:16:01 -0500

    j

--- Assets/Week 2/Scripts/Switch.cs
+++ Assets/Week 2/Scripts/Switch.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Switch : MonoBehaviour
-{
+{ 
     public SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
