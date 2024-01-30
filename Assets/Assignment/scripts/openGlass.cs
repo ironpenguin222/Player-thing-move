bfcc2b4d0651afc8910500baeb88e03373281dca
pocockm <pocockm@OA-S145-01>
2024-01-30 14:47:36 -0500

    finished

--- Assets/Assignment/scripts/openGlass.cs
+++ Assets/Assignment/scripts/openGlass.cs
@@ -6,9 +6,10 @@ public class openGlass : MonoBehaviour
 {
     public GameObject glass;
     private void OnCollisionEnter2D(Collision2D collision)
-        {
+    {
 
         glass.SetActive(!glass.activeInHierarchy);
 
-        }
+    }
+}
     
